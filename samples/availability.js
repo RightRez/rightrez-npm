f1cf8949d7dec420de88ea31bd620245fc7ffd33
Nathan Ainslie <nainslie@rightrez.com>
2015-04-10 14:30:12 -0400

    whitespace and indenting

--- samples/availability.js
+++ samples/availability.js
@@ -6,33 +6,33 @@ var sample_options = require('./sample-config').sample_options;
 api.configure(sample_options);
 
 var request = {
-	"ClientId": sample_options.client_id,
-	"TripComponents":[
+    "ClientId": sample_options.client_id,
+    "TripComponents":[
 	{
-		"DepartCity":"JFK",
-		"ArriveCity":"LHR",
-		"AirDate":"14Jun15",
-		"TripDirection":"OUTBOUND_TRIP",
+	    "DepartCity":"JFK",
+	    "ArriveCity":"LHR",
+	    "AirDate":"14Jun15",
+	    "TripDirection":"OUTBOUND_TRIP",
 	},
 	{
-		"DepartCity":"LHR",
-		"ArriveCity":"JFK",
-		"AirDate":"21Jun15",
-		"TripDirection":"RETURN_TRIP",
+	    "DepartCity":"LHR",
+	    "ArriveCity":"JFK",
+	    "AirDate":"21Jun15",
+	    "TripDirection":"RETURN_TRIP",
 	}],
-	"Options":
-	{
-		"BookingType":"AirSea",
-	}
+    "Options":
+    {
+	"BookingType":"AirSea",
+    }
 };
 
 
 api.availability.search(request, function (err, response) {
-	if (err) {
-		//  error
-		console.log('ERROR: ' + err)
-		return;	
-	}
-
-	console.log('RESPONSE: ' + JSON.stringify(response))
+    if (err) {
+	//  error
+	console.log('ERROR: ' + err)
+	return;	
+    }
+    
+    console.log('RESPONSE: ' + JSON.stringify(response))
 });
